94c4ebfe69ae1b3e4b2874437ee5b943e78bd66f
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-02-07 22:58:30 +0700

    fix

---
+++ Calculator/script.js
@@ -0,0 +1,21 @@
+const $display = document.querySelector('input[type="text"]');
+const $button = document.querySelectorAll('input[type="button"]');
+
+$button.forEach(($button) => {
+    $button.addEventListener('click', ()=> {
+        const value = $button.value;
+
+        if (value === 'C') {
+            $display.value = '';
+            return;
+        }
+
+        if(value === '=') {
+            const express = $display.value.replace(/X/g, '*');
+            $display.value = eval(express);
+            return;
+        }
+
+        $display.value += value;
+    })
+})
