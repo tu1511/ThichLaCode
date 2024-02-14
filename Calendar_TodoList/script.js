532480e06b722bcfedd19f5528203bfad5661b96
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-02-15 00:25:18 +0700

    push

--- Calendar_TodoList/script.js
+++ Calendar_TodoList/script.js
@@ -13,19 +13,19 @@ function generateCalendar() {
     const lastDayOfMonth = new Date(year, month+1, 0);
 
     const firstDayOfWeek = firstDayOfMonth.getDay();
-    const lastDayOfWeek = lastDayOfMonth.getDate();
+    const totalDays = lastDayOfMonth.getDate();
 
     for (let i = 0; i < firstDayOfWeek; i++) {
         let blankDay = document.createElement("div");
         calendar.appendChild(blankDay);
     }
 
-    for (let day = 1; day <= tatolDays; day++) {
+    for (let day = 1; day <= totalDays; day++) {
         let daySquare = document.createElement("div");
         daySquare.className = "calendar-day";
         daySquare.textContent = day;
         daySquare.id = `day-${day}`;
-        calendar.appendChild(daySquare)
+        calendar.appendChild(daySquare);
     }
 }
 
@@ -40,6 +40,14 @@ function closeAddTaskModal () {
 function deleteTask (taskElement) {
     if (confirm("Are you sure you want to delete this task?")) {
         taskElement.parentNode.removeChild(taskElement);
+    }
+}
+
+function editTask(taskElement) {
+    const newTaskDesc = prompt("Edit your task: ", taskElement.textContent);
+
+    if(newTaskDesc !== null & newTaskDesc.trim() !== "") {
+        taskElement.textContent = newTaskDesc;
     }
 }
 
